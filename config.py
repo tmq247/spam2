a36e7bbd1c84e9f955c0e80473c7218d27af743a
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-10-13 15:00:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 """ General config """
 # Set ENV to any value to use webhook instead of polling for bot. Must be set in prod environment.
 ENV = getenv("ENV")
-TZ_OFFSET = 7.0  # (UTC+08:00)
+TZ_OFFSET = 8.0  # (UTC+08:00)
 JOB_LIMIT_PER_PERSON = 10
 BATCH_SIZE = 100  # Max number of messages to send at any given time
 RETRIES = 1  # Number of retries if message fails to send
